9c08c3eccef8c2ec4899a5d357bbf58e8c894500
Joseph Nady <jozephnady@gmail.com>
2023-03-22 16:29:12 -0700

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * jack_bauer - unction that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
- *
- * Return: result
+ * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
+ * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
+ * and resets mins
  */
 void jack_bauer(void)
 {
