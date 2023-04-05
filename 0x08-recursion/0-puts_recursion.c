164913f03809cbca423929e3acd7b602dbbbdff6
Joseph Nady <jozephnady@gmail.com>
2023-04-05 04:21:18 -0700

    new commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line.
+ * @s: string
  * Return: recusive function
  */
 void _puts_recursion(char *s)
 {
-    printf("%c",*s);
+	printf("%c",*s);
 
-    if (*s)
-    {
-        _puts_recursion(s + 1);
-    }
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+	}
 }
