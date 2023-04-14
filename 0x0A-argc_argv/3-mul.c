533305dc3fcb76587ce5ea8cd51f0fbb2186484b
Joseph Nady <jozephnady@gmail.com>
2023-04-14 09:55:45 -0700

    new commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,8 +12,8 @@ int main(int argc, char **argv)
 {
 	int res, x, y;
 
-	if( argc > 3)
-		printf("%s\n","Error, Add two arg only");
+	if (argc > 3)
+		printf("%s\n", "Error, Add two arg only");
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 	res = x * y;
