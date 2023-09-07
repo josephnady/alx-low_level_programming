eca2c826a38a5f7367503ce600ba8914d55a805e
josephnady <jozephnady@gmail.com>
2023-09-08 00:13:27 +0200

    updatess33ww

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,25 +11,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+	int o, w, len = 0;
 
-if (filename == NULL)
-{
-return (-1);
-}
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
-len++;
-}
+	if (filename == NULL)
+		return (-1);
 
-o = open(filename, O_APPEND | O_WRONLY);
-w = write(o, text_content, len);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-if (o == -1 || w == -1)
-return (-1);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-close(o);
+	if (o == -1 || w == -1)
+		return (-1);
 
-return (1);
+	close(o);
+
+	return (1);
 }
