7ca30c95a68931f3d1d801ce88e1dd2f9d60cb88
josephnady <jozephnady#gmail.com>
2024-04-18 16:31:54 -0700

    task1

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * add_dnodeint - func to append new node to the dlinklist
+ * @head: pointer tot hte first node in th dlinklist
+ * @n: the data to be stored in the new node
+ * Return: pointer to the new node
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
