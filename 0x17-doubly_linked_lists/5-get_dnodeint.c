e469ae873d888a7f7ad5bc43ddbfe5ed690e9044
josephnady <jozephnady#gmail.com>
2024-04-19 04:32:20 -0700

    task6

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,18 +1,29 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -  func
- * @head: pointer
- * @index: index
- * Retrun: Node
+ * get_dnodeint_at_index - get the node at selected index
+ * @head: pointer to the first node
+ * @index: index for the node to be got
+ * Return: Node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
+	unsigned int len = 0;
+	dlistint_t * temp;
 
 	if (head == NULL)
 		return (NULL);
-
+	
+	temp = head;
+	while (temp->next != NULL)
+	{
+		len++;
+		temp = temp->next;
+	}
+	if (index > len)
+		return (NULL);
+	
 	for (i = 0; i < index && head->next != NULL; i++)
 	{
 		head = head->next;
