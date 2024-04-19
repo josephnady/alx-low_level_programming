34abbad05c4c5d6574d9e9e019b797d2d30e2dbd
josephnady <jozephnady#gmail.com>
2024-04-19 04:48:45 -0700

    task7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint.c - sum of all the data (n) of a dlistint_t linked list
+ * @head: pointer to  the first node
+ * Return: sum of all values
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	int n = 0;
+
+	if (head == NULL)
+		return (0);
+
+	temp = head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		n += temp->n;
+	}
+	return (n);
+}
