4e59ae94651ec0a87d45d2bc12037b15cccce2fd
josephnady <jozephnady#gmail.com>
2024-04-19 04:35:03 -0700

    task6

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,11 +10,11 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 	unsigned int len = 0;
-	dlistint_t * temp;
+	dlistint_t *temp;
 
 	if (head == NULL)
 		return (NULL);
-	
+
 	temp = head;
 	while (temp->next != NULL)
 	{
@@ -23,7 +23,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	}
 	if (index > len)
 		return (NULL);
-	
+
 	for (i = 0; i < index && head->next != NULL; i++)
 	{
 		head = head->next;
