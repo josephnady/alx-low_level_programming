359f3fb9894ec25490a3530a53d4f38585810e70
josephnady <jozephnady#gmail.com>
2024-04-20 06:18:51 -0700

    task8 correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (; idx != 1; idx--)
+	for (; idx != 0; idx--)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
