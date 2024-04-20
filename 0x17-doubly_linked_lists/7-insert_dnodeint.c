4b4b2e7f45ef0e5372bdc82f33e22ed543e9edb9
josephnady <jozephnady#gmail.com>
2024-04-19 18:06:57 -0700

    new solution for task 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,43 +8,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp, *new, *cur;
-	unsigned int len = 0, i;
+	dlistint_t *tmp = *h, *new;
 
-	cur = *h;
-	while (cur != NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		len++;
-		cur = cur->next;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
+
 	new->n = n;
-	if (idx == 0)
-	{
-		free(new);
-		return (add_dnodeint(h, n));
-	}
-	if (idx > len)
-	{
-		free(new);
-		return (NULL);
-	}
-	if (idx == len)
-	{
-		free(new);
-		return (add_dnodeint_end(&(*h), n));
-	}
-	tmp = *h;
-	for (i = 0; i < idx && tmp != NULL; i++)
-		tmp = tmp->next;
-	new->prev = tmp->prev;
-	tmp->prev->next = new;
-	new->next = tmp;
-	tmp->prev = new;
-	return (*h);
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
