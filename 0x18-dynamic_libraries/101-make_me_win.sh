019d5d16f25ff3651f223126d9e556fc1f9123c1
josephnady <jozephnady#gmail.com>
2024-06-01 10:00:28 -0700

    adv

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp_test https://github.com/josephnady/alx-low_level_programming/blob/main/0x18-dynamic_libraries/librand_override.so
+wget -P /tmp_test https://raw.githubusercontent.com/josephnady/alx-low_level_programming/blob/main/0x18-dynamic_libraries/librand_override.so
 export LD_PRELOAD=/tmp_test/librand_override.so
